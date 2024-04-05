925033224a2966864abfd7fc358ed98ff03fe899
Lorenz Nimmervoll <admin@nimmervoll.work>
2024-04-05 16:45:36 +0200

    fix: discount generator output

--- infrastructure/lib/buttonize-app.ts
+++ infrastructure/lib/buttonize-app.ts
@@ -1,23 +1,9 @@
 import { Construct } from 'constructs'
-// import { LlrtFunction } from './factories/llrt-function'
+import { LlrtFunction } from './factories/llrt-function'
 import { Action, ButtonizeApp, Display, Input } from 'buttonize/cdk'
-import { aws_lambda, aws_lambda_nodejs } from 'aws-cdk-lib'
-import path from 'path'
 
 export const createButtonizeAdminApp = (scope: Construct) => {
-  // const discountGenerator = LlrtFunction(scope, 'discount-generator')
-
-  const discountGenerator = new aws_lambda_nodejs.NodejsFunction(
-    scope,
-    'discount-generator',
-    {
-      entry: path.join(
-        __dirname,
-        '../../backend/src/discount-generator.fn/index.ts'
-      ),
-      runtime: aws_lambda.Runtime.NODEJS_20_X,
-    }
-  )
+  const discountGenerator = LlrtFunction(scope, 'discount-generator')
 
   new ButtonizeApp(scope, 'DemoApp', {
     name: 'Discount code generator',
@@ -50,8 +36,8 @@ export const createButtonizeAdminApp = (scope: Construct) => {
       body: [
         Display.heading('Last Updated: ' + new Date().toLocaleString()),
         Display.heading('Discount generated'),
-        Display.text('Discount value: {{InputPage.discount.value}}'),
-        Display.text('Discount code: {{InputPage.discountGenerator.code}}'),
+        Display.text('Discount value: {{discount.value}}'),
+        Display.text('Discount code: {{discountGenerator.code}}'),
       ],
     })
 }
